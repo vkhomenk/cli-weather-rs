b0aa37bfa4d539cedd02b80b17df5504bd509af8
username <username@localhost.localdomain>
2023-04-10 10:40:33 +0300

    added some mode doc comments

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
-//! # weather
+//! CLI weather
 //! 
+//! This programm provides a convenient way to request weather forecast
+//! from a chosen provider. Configure providers by setting API keys, set
+//! which one to use by default and get weather forecast for up to 5 days ahead.
 
 mod cli;
 mod config;
